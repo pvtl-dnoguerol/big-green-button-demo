19fa9ca51de8fbf3fcbd91f017da9933a3e5d48d
Dan Noguerol <dnoguerol@pivotal.io>
2018-09-27 07:50:16 +0200

    Update

--- ci/build-www.sh
+++ ci/build-www.sh
@@ -3,10 +3,4 @@ cd big-green-www/www
 ln -s /opt/node_modules node_modules
 echo "export const environment = { production: true, apiPrefix: '$API_PREFIX', mapsApiKey: '$GOOGLE_MAPS_API_KEY' };" > src/environments/environment.prod.ts
 ng build --source-map=false --configuration=production --preserve-symlinks
-ng build --configuration=production-en --preserve-symlinks
-ng build --configuration=production-nl --preserve-symlinks
 cp -rf dist/www/* ../../build-output2/
-mkdir ../../build-output2/en
-cp -rf dist/www-en/* ../../build-output2/en/
-mkdir ../../build-output2/nl
-cp -rf dist/www-nl/* ../../build-output2/nl/
